9849b7bd41d2843ff9d2383d7684f14330b20a84
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2020-12-18 17:20:44 +0900

    refactor page entry calculation

--- kernel/paging.cpp
+++ kernel/paging.cpp
@@ -14,14 +14,25 @@ namespace {
   alignas(kPageSize4K) std::array<uint64_t, 512> pdp_table;
   alignas(kPageSize4K)
     std::array<std::array<uint64_t, 512>, kPageDirectoryCount> page_directory;
+
+  uint64_t toPresent(uint64_t entry) {
+      return entry | 0b0001;
+  }
+  uint64_t toWritable(uint64_t entry) {
+      return entry | 0b0010;
+  }
+  uint64_t toHugePage(uint64_t entry) {
+      return entry | 0b1000'0000;
+  }
 }
 
+
 void SetupIdentityPageTable() {
-  pml4_table[0] = reinterpret_cast<uint64_t>(&pdp_table[0]) | 0x003;
+  pml4_table[0] = toWritable(toPresent(reinterpret_cast<uint64_t>(&pdp_table[0])));
   for (int i_pdpt = 0; i_pdpt < page_directory.size(); ++i_pdpt) {
-    pdp_table[i_pdpt] = reinterpret_cast<uint64_t>(&page_directory[i_pdpt]) | 0x003;
+    pdp_table[i_pdpt] = toWritable(toPresent(reinterpret_cast<uint64_t>(&page_directory[i_pdpt])));
     for (int i_pd = 0; i_pd < 512; ++i_pd) {
-      page_directory[i_pdpt][i_pd] = i_pdpt * kPageSize1G + i_pd * kPageSize2M | 0x083;
+        page_directory[i_pdpt][i_pd] = toWritable(toPresent(toHugePage(i_pdpt * kPageSize1G + i_pd * kPageSize2M)));
     }
   }
 
